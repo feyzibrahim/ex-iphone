35c0f1a472739633f0c2bc90b1b9d61365332937
Feyz Ibrahim <faizashrafkk@gmail.com>
2023-12-04 13:59:30 +0530

    ui bug fixed

--- client/src/page/admin/pages/Settings.jsx
+++ client/src/page/admin/pages/Settings.jsx
@@ -1,9 +1,39 @@
 import React from "react";
+import { Chart as ChartJS } from "chart.js";
+import { Pie } from "react-chartjs-2";
 
 const Settings = () => {
+  const data = {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          "rgba(255, 99, 132)",
+          "rgba(54, 162, 235)",
+          "rgba(255, 206, 86)",
+          "rgba(75, 192, 192)",
+          "rgba(153, 102, 255)",
+          "rgba(255, 159, 64)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
   return (
     <div className="p-5">
       <h1 className="font-bold text-2xl">Settings</h1>
+      <Pie data={data} />
     </div>
   );
 };
